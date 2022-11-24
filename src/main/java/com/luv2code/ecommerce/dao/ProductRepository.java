22fd51772bbe6581b5adcca45875881bf97ad062
Wang Qin <wangqin0@outlook.com>
2022-11-24 11:48:37 -0800

    Search for Products by Category, static - 4. Add `findByCategoryId` to `ProductRepository` to provide a new endpoint

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    // Spring will execute a query similar to:
+    //  `SELECT * FROM product WHERE category_id={id}`
+    // And automatically exposes endpoint: /api/products/search/findByCategoryId?id={id}
 }
