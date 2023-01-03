4df047334014af512d54499149fdc41d5341cdb6
Wang Qin <wangqin0@outlook.com>
2023-01-03 10:53:34 -0800

    Order History 0. Fix: `dataCreated` -> `dateCreated` 7. findByCustomerEmailOrderByDateCreatedDesc

--- src/main/java/com/luv2code/ecommerce/entity/Order.java
+++ src/main/java/com/luv2code/ecommerce/entity/Order.java
@@ -36,7 +36,7 @@ public class Order {
 
     @Column(name = "date_created")
     @CreationTimestamp
-    private Date dataCreated;
+    private Date dateCreated;
 
     @Column(name = "last_updated")
     @UpdateTimestamp
