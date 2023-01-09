11574e0ccd934ff68844383f2faff190ddbc215e
Wang Qin <wangqin0@outlook.com>
2023-01-08 23:17:21 -0800

    Stripe Email Receipts 1. Add email field to `PaymentInfo.java` 2. In `createPaymentIntent`, assign email address

--- src/main/java/com/luv2code/ecommerce/dto/PaymentInfo.java
+++ src/main/java/com/luv2code/ecommerce/dto/PaymentInfo.java
@@ -8,4 +8,6 @@ public class PaymentInfo {
     private int amount;
     private String currency;
 
+    private String receiptEmail;
+
 }
--- src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -83,6 +83,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
         params.put("description", "Wang's Shop");
+        params.put("receipt_email", paymentInfo.getReceiptEmail());
 
         return PaymentIntent.create(params);
     }
